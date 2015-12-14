d60f7011c97ec84da1cfb3746962f778f681e82e
Sakamoto <sakamoto144yuta@gmail.com>
2015-12-14 17:34:43 +0900

    よくわからないものを作りました

--- app/src/main/java/jushin/net/memoryfresh/MainActivity.java
+++ app/src/main/java/jushin/net/memoryfresh/MainActivity.java
@@ -1,5 +1,6 @@
 package jushin.net.memoryfresh;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +17,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = new Button(this);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = getIntent();
+                intent.setClass(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
