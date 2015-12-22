b6056693a2163eae811761184978f476361e1c0b
小橋勇太 <Yuta@kobashiyuuta-no-MacBook-Pro.local>
2015-12-22 12:12:49 +0900

    android5.0から仕様が変更され、実行中のプロセスを取得しにくくなっています。 ライブラリを使用しました。https://github.com/jaredrummler/AndroidProcesses この方法ではandroid4.4以下では実行中のプロセスは取得できません。

--- app/src/main/java/jushin/net/memoryfresh/MainActivity.java
+++ app/src/main/java/jushin/net/memoryfresh/MainActivity.java
@@ -9,24 +9,38 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import com.jaredrummler.android.processes.ProcessManager;
+import com.jaredrummler.android.processes.models.AndroidAppProcess;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> process_names;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = new Button(this);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = getIntent();
-                intent.setClass(getBaseContext(), MainActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
+        listView = (ListView)findViewById(R.id.listview);
+        List<AndroidAppProcess> processes = ProcessManager.getRunningAppProcesses();
+        process_names = new ArrayList<String>();
 
+        for (AndroidAppProcess process : processes) {
+            String processName = process.name;
+            process_names.add(processName);
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_expandable_list_item_1, process_names);
+
+        listView.setAdapter(adapter);
+    }
 }
