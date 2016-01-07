444a0e1258c849201462a06f2de0dc7f6d62f9b0
freedom <zgmf_x1oa_freedom18@yahoo.co.jp>
2016-01-07 19:08:11 +0900

    バグ修正

--- app/src/main/java/jushin/net/memoryfresh/GraphFragment.java
+++ app/src/main/java/jushin/net/memoryfresh/GraphFragment.java
@@ -1,21 +1,15 @@
 package jushin.net.memoryfresh;
 
-import android.app.Fragment;
-import android.content.pm.PackageManager;
-import android.graphics.drawable.Drawable;
+
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
 
-import com.jaredrummler.android.processes.ProcessManager;
-import com.jaredrummler.android.processes.models.AndroidAppProcess;
-
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 
 /**
@@ -52,55 +46,16 @@ public class GraphFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View v = inflater.inflate(R.layout.fragment_main, container, false);
+        View v = inflater.inflate(R.layout.fragment_graph, container, false);
 
-        listView = (ListView)v.findViewById(R.id.listview);
-        List<AndroidAppProcess> processes = ProcessManager.getRunningAppProcesses();
+        listView = (ListView)v.findViewById(R.id.graph);
 
-        List<ListItem> items = new ArrayList<ListItem>();
-
-        Drawable icon = null;
-        PackageManager pm = getContext().getPackageManager();
-
-        for (AndroidAppProcess process : processes) {
-
-            ListItem listItem = new ListItem();
-
-            String processName = process.name;
-            long size = 0L;
-            try {
-                size = process.statm().getSize();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            try {
-                //アプリのアイコン取得
-                icon = pm.getApplicationIcon(process.name);
-            } catch (PackageManager.NameNotFoundException e) {
-
-                e.printStackTrace();
-                //例外が発生した場合デフォルトのアイコンを適用
-                icon = getContext().getDrawable(R.drawable.ic_launcher);
-            }
-
-            listItem.setText(processName,size / 8 / 1000 + "KB");
-            listItem.setImageId(icon);
-
-            items.add(listItem);
-        }
-
-        // adapterのインスタンスを作成カスタムレイアウトを適用
-        ProcessListAdapter adapter =
-                new ProcessListAdapter(getContext(),R.layout.list_item, items);
-
-        listView.setAdapter(adapter);
 
         return v;
     }
 
 
-//   
+//
 
 
 
