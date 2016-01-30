adb7f0109596800ad035962d5a88c63cea12d792
小橋勇太 <Yuta@kobashiyuuta-no-MacBook-Pro.local>
2016-01-30 20:44:10 +0900

    データベース関連ファイル

--- app/src/main/java/jushin/net/memoryfresh/activity/SettingsActivity.java
+++ app/src/main/java/jushin/net/memoryfresh/activity/SettingsActivity.java
@@ -2,12 +2,17 @@ package jushin.net.memoryfresh.activity;
 
 
 import android.app.FragmentTransaction;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.WindowManager;
 
+import java.util.ArrayList;
+
 import jushin.net.memoryfresh.R;
+import jushin.net.memoryfresh.database.ProcessManageDBHelper;
 
 public class SettingsActivity extends AppCompatActivity {
     Toolbar toolbar;
@@ -23,5 +28,30 @@ public class SettingsActivity extends AppCompatActivity {
 
 
 
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+
+        //チェックされていたアイテムをINSERTする
+        ArrayList<String> checkedArrayList = new ArrayList<String>;
+
+        //チェックボックスをつけたアプリケーションのパッケージ名を保存
+        SQLiteDatabase database;
+        ProcessManageDBHelper dbHelper = new ProcessManageDBHelper(getApplicationContext());
+        database = dbHelper.getReadableDatabase();
+        //一旦データベースの内容を全て削除
+        //第一引数：テーブル名
+        //第二引数、第三引数：SQLのWHERE条件文を作成するための引数。
+        database.delete("freshtable", null, null);
+
+
+        //checkedArrayList = slectCheckBoxEnables(getApplicationContext());
+
+        for(String packageName: checkedArrayList){
+            String sql = "INSERT (package) INTO freshtable VALUES('" + packageName + "')";
+
+        }
     }
 }
